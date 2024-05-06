f37fdeda3763754d157ae4bf049891fc0e2a015f
jana-trpkovska <107814974+jana-trpkovska@users.noreply.github.com>
2024-05-06 17:43:07 +0200

    Add lab4 gr A

---
+++ Lab4/MovieApp/EShop.Web/Controllers/API/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using EShop.Domain.Domain;
+using EShop.Domain.Identity;
+using EShop.Service.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+        private readonly UserManager<EShopApplicationUser> userManager;
+        public AdminController(IOrderService orderService, UserManager<EShopApplicationUser> userManager)
+        {
+            _orderService = orderService;
+            this.userManager = userManager;
+        }
+
+        [HttpGet("[action]")]
+        public List<Order> GetAllOrders()
+        {
+            return _orderService.GetAllOrders();
+        }
+
+        [HttpPost("[action]")]
+        public Order GetDetailsForOrder(BaseEntity model)
+        {
+            return _orderService.GetDetailsForOrder(model);
+        }
+    }
+}
